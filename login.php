3a16cf1dc970e7f39c2a22932c981ecf82bdfde8
Fanny A <fan.a76@gmail.com>
2021-03-01 19:46:36 +0100

    fichiers pour page de connexion

---
+++ login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Login</title>
+</head>
+<body>
+    <form action="validation.php" method="post" class="boxLogin">
+        <h2>Connexion</h2>
+        <input type="text" name="nom" placeholder="Nom d'utilisateur">
+        <input type="password" name="mot_de_passe" placeholder="Mot de passe">
+        <input type="submit" value="Continuer">
+        <img src="images/logo_ifaMakerSmall.jpg" alt="logo ifaMaker" class="logoIfaMaker">
+    </form>
+</body>
+</html>
